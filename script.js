523dc82855ec765daa1c49d8d50653fa6483533f
samiandreas70@gmail.com <samiandreas70@gmail.com>
2023-04-12 13:55:39 -0400

    added tabs and also added functionality to tabs to hide and show based on clicks.

---
+++ script.js
@@ -0,0 +1,18 @@
+//setting variables for the class names to get the 
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+// this function's purpose is to hide the underline and also the text content under each heading 
+function opentab(tabName){
+    for(tablink of tablinks){
+    //this is to remove the underline under each header 
+        tablink.classList.remove("active-link");
+}
+    for(tabcontent of tabcontents){
+    //this is to remove the text content for each header 
+        tabcontent.classList.remove("active-tab");    
+}
+    event.currentTarget.classList.add("acitve-link")
+    document.getElementById(tabName).classList.add("active-tab");
+}
